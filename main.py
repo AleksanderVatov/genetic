7d78de091067f8ac0bf22388ff23299e43981768
Yann21 <hyann98@gmail.com>
2019-12-08 14:26:39 +0100

    Boilerplate

--- main.py
+++ main.py
@@ -1,8 +1,92 @@
+from deap import base, creator, tools
+
 import blackbox
 import random
 
+N_SLICES = 128
+
 oracle = blackbox.BlackBox('shredded.png', 'original.png')
-p = list(range(128))
-random.shuffle(p)
-print(oracle.evaluate_solution(p))  # Print the solution's fitness
-# oracle.show_solution(p) # Display the solution
+
+class Strategy(object):
+    'Strategy to find the correct permutation.'
+    def __init__(self):
+        pass
+
+    def random(self):
+        order = list(range(N_SLICES))
+        random.shuffle(order)
+        return order
+    
+    def identity(self):
+        return list(range(N_SLICES))
+
+def deap_tutorial(test: str) -> None:
+    # Type creation / TensorFlow-like
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMin)
+
+    # Initialization of your variables
+    IND_SIZE = 10
+
+    toolbox = base.Toolbox()
+    toolbox.register("attribute", random.random)
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
+                    toolbox.attribute, n=IND_SIZE)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    # The fun part
+    def evaluate(individual):
+        return sum(individual)
+
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("evaluate", evaluate)
+
+    # Algorithm
+    def main():
+        pop = toolbox.population(n=50)
+        CXPB, MUTPB, NGEN = 0.5, 0.2, 40
+
+        # Evaluate the entire population
+        fitnesses = map(toolbox.evaluate, pop)
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit
+
+        for g in range(NGEN):
+            # Select the next generation individuals
+            offspring = toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            offspring = map(toolbox.clone, offspring)
+
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < CXPB:
+                    toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+                if random.random() < MUTPB:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+
+            # The population is entirely replaced by the offspring
+            pop[:] = offspring
+
+        return pop
+
+if __name__ == '__main__':
+    strat = Strategy()
+    order = strat.random()
+
+    # Print the solution's fitness
+    print(oracle.evaluate_solution(order))
+    # Display the solution
+    oracle.show_solution(order)
