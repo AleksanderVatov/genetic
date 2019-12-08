94033ca1db366dbb93e1014f472e681e37341e6c
Yann21 <hyann98@gmail.com>
2019-12-08 13:48:42 +0100

    All hail the autoformat

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import random
 oracle = blackbox.BlackBox('shredded.png', 'original.png')
 p = list(range(128))
 random.shuffle(p)
-print(oracle.evaluate_solution(p)) # Print the solution's fitness
+print(oracle.evaluate_solution(p))  # Print the solution's fitness
 # oracle.show_solution(p) # Display the solution
